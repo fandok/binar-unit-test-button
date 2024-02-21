1cb039678181c8a9e3454d66ba6e97444a78529e
fandok <fandika.okdiba@gmail.com>
2024-02-21 21:17:31 +0700

    unit test exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
 const App = () => {
-  return <div>App</div>;
+  const [text, setText] = useState("");
+
+  const handleClick = () => {
+    console.log("testing");
+  };
+
+  return (
+    <div>
+      Main PS
+      <button onClick={handleClick}>Mau main!</button>
+      <button
+        onClick={() => {
+          setText("suka dong");
+        }}
+      >
+        Suka gak?
+      </button>
+      <div>{text ? text : "gak suka"}</div>
+    </div>
+  );
 };
 
 export default App;
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,12 +1,43 @@
-import { render, screen } from "@testing-library/react";
-import { describe, expect, it } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
 
 import App from "./App";
 
 describe("App page", () => {
-  it("renders page and get App text", () => {
+  it("renders App page and get title text", () => {
     render(<App />);
-    const appText = screen.getByText("App");
-    expect(appText).toBeInTheDocument();
+    expect(screen.getByText("Main PS")).toBeInTheDocument();
+  });
+
+  it("renders App page and click button", () => {
+    render(<App />);
+
+    const buttonText = screen.getByText("Mau main!");
+    expect(buttonText).toBeInTheDocument();
+
+    const logSpy = vi.spyOn(console, "log");
+    fireEvent.click(buttonText);
+    expect(logSpy).toHaveBeenCalledWith("testing");
+  });
+
+  it("renders App page, click suka button then get the new text", () => {
+    render(<App />);
+
+    const sukaButton = screen.getByText("Suka gak?");
+    expect(sukaButton).toBeInTheDocument();
+
+    let sukaText = screen.getByText("gak suka");
+    expect(sukaText).toBeInTheDocument();
+
+    let newSukaText = screen.queryByText("suka dong");
+    expect(newSukaText).not.toBeInTheDocument();
+
+    fireEvent.click(sukaButton);
+
+    newSukaText = screen.getByText("suka dong");
+    expect(newSukaText).toBeInTheDocument();
+
+    sukaText = screen.queryByText("gak suka");
+    expect(sukaText).not.toBeInTheDocument();
   });
 });
